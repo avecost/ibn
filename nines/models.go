f3fca2f58d6f96b679c4153727ded2c356c32711
Arvin San Andres <arvinsanandres@gmail.com>
2017-10-17 15:30:55 +0800

    Import SG8 Baccarat Natural 9 v1.0

--- nines/models.go
+++ nines/models.go
@@ -23,12 +23,6 @@ type Nine struct {
 }
 
 func InsertNine(stmt *sql.Stmt, n Nine) {
-	//stmt, err := config.DB.Prepare("INSERT INTO nines (login, game_name, bet_banker, bet_player, bet_tie, total_payout, game_number, game_time, dealer_cards, player_cards, banker_n9, player_n9) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer stmt.Close()
-
 	_, err := stmt.Exec(n.Login, n.GameName, n.BetBanker, n.BetPlayer, n.BetTie, n.TotalPayout, n.GameNumber, n.GameTime, n.DealerCards, n.PlayerCards, n.BankerNine, n.PlayerNine)
 	if err != nil {
 		log.Fatal(err.Error())
